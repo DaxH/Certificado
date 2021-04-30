b9d6567c3e5f2f053594300a876c2416f3cf6922
DaxH <dari.xavi.bri@gmail.com>
2021-04-30 17:08:26 -0500

    r

--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from firebase_admin import firestore
 # Create your views here.
 def index(request):
+    print(f"FIREBASE CONFIG {settings.FIREBASE_CONFIG}")
     if not firebase_admin._apps:
         firebase = firebase_admin.initialize_app(settings.FIREBASE_CONFIG)
     db = firestore.Client()
